49b478adb5af99cf7185aaecf9eef13d6e614eea
voelter <voelter@ea8f12be-636b-2986-791d-faaaf190aeea>
2010-08-24 15:20:43 +0000

    initial commit

---
+++ trunk/expr/src-gen/expr/exprDemo/Model.java
@@ -0,0 +1,47 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+
+ */
+package expr.exprDemo;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Model</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link expr.exprDemo.Model#getElements <em>Elements</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see expr.exprDemo.ExprDemoPackage#getModel()
+ * @model
+ * @generated
+ */
+public interface Model extends EObject
+{
+  /**
+   * Returns the value of the '<em><b>Elements</b></em>' containment reference list.
+   * The list contents are of type {@link expr.exprDemo.Element}.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Elements</em>' containment reference list isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Elements</em>' containment reference list.
+   * @see expr.exprDemo.ExprDemoPackage#getModel_Elements()
+   * @model containment="true"
+   * @generated
+   */
+  EList<Element> getElements();
+
+} // Model
