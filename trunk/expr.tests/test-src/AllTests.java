fa25cc3cbcb45c4cb190543013b3c4234479afb2
voelter <voelter@ea8f12be-636b-2986-791d-faaaf190aeea>
2010-08-24 15:26:34 +0000

    initial commit

---
+++ trunk/expr.tests/test-src/AllTests.java
@@ -0,0 +1,23 @@
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import tests.Array;
+import tests.Basic;
+import tests.Enum;
+import tests.Subtyping;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	Basic.class,
+	Subtyping.class,
+	Enum.class,
+	Array.class
+})
+public class AllTests {
+
+}
+   
+
