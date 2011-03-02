6f3d790f2e6cda8ceb576f90da6213023d79cab1
voelter <voelter@ea8f12be-636b-2986-791d-faaaf190aeea>
2011-03-02 18:13:58 +0000

    adapted to new logview API

--- expr.ui/src/expr/ui/quickfix/ExprDemoQuickfixProvider.java
+++ expr.ui/src/expr/ui/quickfix/ExprDemoQuickfixProvider.java
@@ -1,17 +1,7 @@
 
 package expr.ui.quickfix;
 
-import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPartSite;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.internal.Workbench;
-import org.eclipse.ui.part.WorkbenchPart;
-import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.editor.model.edit.IModification;
 import org.eclipse.xtext.ui.editor.model.edit.IModificationContext;
 import org.eclipse.xtext.ui.editor.quickfix.DefaultQuickfixProvider;
@@ -19,10 +9,7 @@ import org.eclipse.xtext.ui.editor.quickfix.Fix;
 import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
 import org.eclipse.xtext.validation.Issue;
 
-import de.itemis.interpreter.logging.LogEntry;
-import de.itemis.interpreter.ui.logview.LogViewHelper;
-import de.itemis.interpreter.ui.logview.LogViewPart;
-
+import de.itemis.interpreter.ui.logview.LogViewUtil;
 import expr.validation.ExprDemoJavaValidator;
 
 public class ExprDemoQuickfixProvider extends DefaultQuickfixProvider {
@@ -32,7 +19,7 @@ public class ExprDemoQuickfixProvider extends DefaultQuickfixProvider {
 		acceptor.accept(issue, "Show Log", "Show the execution Log of the Test", "upcase.png", new IModification() {
 			@SuppressWarnings("restriction")
 			public void apply(IModificationContext context) throws BadLocationException {
-				LogViewHelper.showMostRecentLog();
+				LogViewUtil.showMostRecentLog(null);
 			}
 		});
 	}
