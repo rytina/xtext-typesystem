978e72f553940cfb3f63ff488f075ebcbf6024bd
voelter <voelter@ea8f12be-636b-2986-791d-faaaf190aeea>
2011-05-13 18:17:56 +0000

    created a branch for 2.0 work

---
+++ branches/https:/svn.codespot.com/a/eclipselabs.org/xtext-typesystem/branches/v20/expr.tests/test-src/tests/Basic.java
@@ -0,0 +1,21 @@
+package tests;
+
+
+import org.eclipse.emf.ecore.EObject;
+import org.junit.Test;
+
+import de.itemis.xtext.typesystem.testing.XTextTestCase;
+import expr.ExprDemoStandaloneSetup;
+
+public class Basic extends XTextTestCase {
+
+	@Test
+	public void testTypesOfParams() throws Exception {
+		EObject root = initializeAndGetRoot(new ExprDemoStandaloneSetup(), 
+				R.modelroot+"/1-basic.expr");
+		assertConstraints( allIssues.errorsOnly().sizeIs(0) ); 
+		
+	}
+	
+	
+}
