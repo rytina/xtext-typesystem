981f075267cbf3942fa23087f51b3bf8f5d79d2f
A.Muelder@googlemail.com <A.Muelder@googlemail.com@ea8f12be-636b-2986-791d-faaaf190aeea>
2014-09-12 15:19:00 +0000

    ordered and unordered compatibilitycheck can use an errormessageprovider

--- branches/SCT/de.itemis.xtext.typesystem/src/de/itemis/xtext/typesystem/checks/EnsureUnorderedCompatibilityCheck.java
+++ branches/SCT/de.itemis.xtext.typesystem/src/de/itemis/xtext/typesystem/checks/EnsureUnorderedCompatibilityCheck.java
@@ -10,38 +10,53 @@ import de.itemis.xtext.typesystem.exceptions.FeatureMustBeSingleValuedException;
 import de.itemis.xtext.typesystem.trace.TypeCalculationTrace;
 
 public class EnsureUnorderedCompatibilityCheck extends AbstractCompatibilityCheck {
-	public EnsureUnorderedCompatibilityCheck(EClass ctxClass, EStructuralFeature feature2) throws FeatureMustBeSingleValuedException  {
-		super( null, "unordered compatibility for "+ctxClass.getName()+"; itself and feature "+feature2.getName(), ctxClass, null, feature2 );
+	public EnsureUnorderedCompatibilityCheck(EClass ctxClass, EStructuralFeature feature2)
+			throws FeatureMustBeSingleValuedException {
+		super((String) null, "unordered compatibility for " + ctxClass.getName() + "; itself and feature "
+				+ feature2.getName(), ctxClass, null, feature2);
 	}
-	public EnsureUnorderedCompatibilityCheck(String errorMessage, EClass ctxClass, EStructuralFeature feature2) throws FeatureMustBeSingleValuedException  {
-		super( errorMessage, "unordered compatibility for "+ctxClass.getName()+"; itself and feature "+feature2.getName(), ctxClass, null, feature2 );
+
+	public EnsureUnorderedCompatibilityCheck(String errorMessage, EClass ctxClass, EStructuralFeature feature2)
+			throws FeatureMustBeSingleValuedException {
+		super(errorMessage, "unordered compatibility for " + ctxClass.getName() + "; itself and feature "
+				+ feature2.getName(), ctxClass, null, feature2);
 	}
-	public EnsureUnorderedCompatibilityCheck(EClass ctxClass, EStructuralFeature feature1, EStructuralFeature feature2) throws FeatureMustBeSingleValuedException  {
-		super( null, "unordered compatibility for "+ctxClass.getName()+"; feature "+feature1.getName()+" and feature "+feature2.getName(), ctxClass, feature1, feature2 );
+
+	public EnsureUnorderedCompatibilityCheck(EClass ctxClass, EStructuralFeature feature1, EStructuralFeature feature2)
+			throws FeatureMustBeSingleValuedException {
+		super((String) null, "unordered compatibility for " + ctxClass.getName() + "; feature " + feature1.getName()
+				+ " and feature " + feature2.getName(), ctxClass, feature1, feature2);
 	}
-	public EnsureUnorderedCompatibilityCheck(String errorMessage, EClass ctxClass, EStructuralFeature feature1, EStructuralFeature feature2) throws FeatureMustBeSingleValuedException  {
-		super( errorMessage, "unordered compatibility for "+ctxClass.getName()+"; feature "+feature1.getName()+" and feature "+feature2.getName(), ctxClass, feature1, feature2 );
+
+	public EnsureUnorderedCompatibilityCheck(String errorMessage, EClass ctxClass, EStructuralFeature feature1,
+			EStructuralFeature feature2) throws FeatureMustBeSingleValuedException {
+		super(errorMessage, "unordered compatibility for " + ctxClass.getName() + "; feature " + feature1.getName()
+				+ " and feature " + feature2.getName(), ctxClass, feature1, feature2);
 	}
-	public boolean check( EObject element, ITypesystem ts, TypeCalculationTrace trace, ValidationMessageAcceptor acceptor, boolean showWarnings ) {
+
+	public boolean check(EObject element, ITypesystem ts, TypeCalculationTrace trace,
+			ValidationMessageAcceptor acceptor, boolean showWarnings) {
 		EObject val1;
-		if ( feature1 != null ) {
+		if (feature1 != null) {
 			val1 = (EObject) element.eGet(feature1);
 		} else {
 			val1 = element;
 		}
 		EObject val2 = (EObject) element.eGet(feature2);
-		if ( val2 != null ) {
+		if (val2 != null) {
 			EObject type1 = ts.typeof(val1, trace.child(feature1 != null ? feature1.getName() : "self", val1));
 			EObject type2 = ts.typeof(val2, trace.child(feature2.getName(), val2));
-			if ( showWarnings && type1 == null ) {
-				trace.add(val1, "type of feature '"+feature1.getName()+"' is undefined");
-				acceptor.acceptWarning("type of feature '"+feature1.getName()+"' is undefined", element, feature1, -1, null);
-			} else if ( showWarnings && type2 == null ){
-				trace.add(val2, "type of featue '"+feature2.getName()+"' is undefined");
-				acceptor.acceptWarning("type of '"+feature2.getName()+"' is undefined", element, feature2, -1, null);
-			} 
-			if ( !ts.isCompatibleTypeUnordered(val1, type1, val2, type2, trace) ) {
-				String m = errorMessage != null ? String.format(errorMessage,type1,type2) : "incompatible type "+ts.typeString(type1)+" and "+ts.typeString(type2)+" (on a "+ctxClass.getName()+")"; 
+			if (showWarnings && type1 == null) {
+				trace.add(val1, "type of feature '" + feature1.getName() + "' is undefined");
+				acceptor.acceptWarning("type of feature '" + feature1.getName() + "' is undefined", element, feature1,
+						-1, null);
+			} else if (showWarnings && type2 == null) {
+				trace.add(val2, "type of featue '" + feature2.getName() + "' is undefined");
+				acceptor.acceptWarning("type of '" + feature2.getName() + "' is undefined", element, feature2, -1, null);
+			}
+			if (!ts.isCompatibleTypeUnordered(val1, type1, val2, type2, trace)) {
+				String m = errorMessage != null ? String.format(errorMessage, type1, type2) : "incompatible type "
+						+ ts.typeString(type1) + " and " + ts.typeString(type2) + " (on a " + ctxClass.getName() + ")";
 				acceptor.acceptError(m, element, feature2, -1, null);
 				return false;
 			}
